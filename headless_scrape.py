950fe6752be8d293b961202ff9795b3b0c9ca203
simon jung <drinkwater0502@gmail.com>
2022-09-12 15:42:47 -0700

    added a headless version of scrape.py so no browser popup

--- headless_scrape.py
+++ headless_scrape.py
@@ -6,11 +6,24 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+from selenium.webdriver.chrome.options import Options
+
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 # PATH = 'C:\Program Files (x86)\chromedriver.exe'
 
 # driver = webdriver.Chrome(PATH)
+
+CHROME_PATH = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+CHROMEDRIVER_PATH = 'C:\Program Files (x86)\chromedriver.exe'
+WINDOW_SIZE = "1920,1080"
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+chrome_options.binary_location = CHROME_PATH
+
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options, service=Service(ChromeDriverManager().install()))
 
 user_card = input('Enter a card number: ')
 
